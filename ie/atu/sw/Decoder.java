632ed93b2018b997e622aae60166d24113fdeda1
Alona S <alona.s.kry@gmail.com>
2025-08-05 00:03:55 +0100

    added basic menu

---
+++ ie/atu/sw/Decoder.java
@@ -0,0 +1,7 @@
+package ie.atu.sw;
+
+public class Decoder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(args[0]);
+    }
+}
--- ie/atu/sw/Menu.java
+++ ie/atu/sw/Menu.java
@@ -3,55 +3,57 @@ package ie.atu.sw;
 import java.util.Objects;
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
+@SuppressWarnings("ALL")
 public class Menu {
     public void renderMenu() throws Exception {
-        System.out.println(ConsoleColour.BLUE);
-        System.out.println("************************************************************");
-        System.out.println("*     ATU - Dept. of Computer Science & Applied Physics    *");
-        System.out.println("*                                                          *");
-        System.out.println("*              Encoding Words with Suffixes                *");
-        System.out.println("*                                                          *");
-        System.out.println("************************************************************");
-        System.out.println("(1) Specify Mapping File");
-        System.out.println("(2) Specify Text File to Encode");
-        System.out.println("(3) Specify Output File (default: ./out.txt)");
-        System.out.println("(4) Configure Options");
-        System.out.println("(5) Encode Text File");
-        System.out.println("(6) Decode Text File");
-        System.out.println("(7) Optional Extras...");
-        System.out.println("(8) Quit");
+        out.println(ConsoleColour.BLUE);
+        out.println("************************************************************");
+        out.println("*     ATU - Dept. of Computer Science & Applied Physics    *");
+        out.println("*                                                          *");
+        out.println("*               Encoding Words with Suffixes               *");
+        out.println("************************************************************");
+        out.println("(1) Specify Mapping File");
+        out.println("(2) Specify Text File to Encode");
+        out.println("(3) Specify Output File (default: ./out.txt)");
+        out.println("(4) Configure Options");
+        out.println("(5) Encode Text File");
+        out.println("(6) Decode Text File");
+        out.println("(7) Optional Extras...");
+        out.println("(8) Quit");
 
-        System.out.print(ConsoleColour.BLACK_BOLD_BRIGHT);
-        System.out.println();
-        System.out.print("Select Option [1-8]>: ");
+        out.print(ConsoleColour.BLACK_BOLD_BRIGHT);
+        out.println();
+        out.print("Select Option [1-8]>: ");
     }
 
     public boolean processMenuInput(boolean keepRunning) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(in);
         Object[][] array = null;
 
         String menuItem = scanner.nextLine();
         switch (menuItem) {
             case "1" -> {
-                System.out.println("Add file location, directory included: 1");
+                out.println("Add file location, directory included: 1");
                 String fileLocation = scanner.nextLine();
 
             }
             case "2" -> {
-                System.out.println("Add file location, directory included: 2");
+                out.println("Add file location, directory included: 2");
                 String fileLocation = scanner.nextLine();
                 FileReader.main(new String[]{fileLocation}, "create");
             }
             case "3" -> {
-                System.out.println("Specify Output File (default: ./out.txt): 3");
+                out.println("Specify Output File (default: ./out.txt): 3");
                 String fileLocation = scanner.nextLine();
             }
             case "8" -> {
-                System.out.println("Goodbye! 8");
+                out.println("Goodbye! 8");
                 keepRunning = false;
             }
             default -> {
-                System.out.println("Invalid Option Selected");
+                out.println("Invalid Option Selected");
             }
         }
         return keepRunning;
